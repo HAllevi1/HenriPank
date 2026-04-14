58f47f98a477efa89cc3532e47caebaeedeb90ee
Henri Allevi <allevi@ut.ee>
2026-04-14 16:17:13 +0300

    refactor(security): externalize JWT secret to application properties
    - Removed hardcoded secret from JwtService to improve security.- Implemented @Value annotation to inject the secret key from application.properties.- Used @PostConstruct hook to ensure secure key initialization after dependency injection.- Decoupled sensitive configuration from the source code.

--- src/main/java/com/example/HenriPank/config/JwtService.java
+++ src/main/java/com/example/HenriPank/config/JwtService.java
@@ -1,20 +1,31 @@
 package com.example.HenriPank.config;
 
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+import java.util.Properties;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET = "minuVagaPikkJaTurvalineSalajaneVotiMidaKeegiEiTea123"; //Hidden in real world, bank secret key used for signing token
-    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    @Value("${jwt.secret}")
+    private String SECRET; //Hidden in real world, bank secret key used for signing token
 
+    private Key key;
+
+    //When spring has gotten its Secret, it will automatically call init to get the KEY
+    @PostConstruct
+    private void init() {
+        this.key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
     /**
      * Generates a secure JWT for a specific user.
      * The token includes the user's email as the subject, issuance time, and an expiration date.
